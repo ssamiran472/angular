c27d5764ce4c88a36cf5e4bc45e690f51b0908ad
samiran <ssamiran472@gmail.com>
2020-04-12 12:33:55 +0530

    some event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,16 @@ export class AppComponent {
     {id:2, name:'course2'},
     {id:3, name:'course3'}
   ]
+
+  addCourse(){
+    let course={id:4, name: 'course4'};
+    this.courses.push(course);
+
+  }
+
+  remove(course){
+    let index = this.courses.indexOf(course);
+    console.log(index);
+    this.courses.splice(index, 1);
+  }
 }
